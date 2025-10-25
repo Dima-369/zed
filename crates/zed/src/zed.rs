3e0424759c3ae81ab371b3599b6d8926bf60698e
Tim Ebben <tim@tjuu.nl>
2025-10-25 09:20:16 +0200

    Add jump navigation feature inspired by flash.nvim

--- crates/zed/src/zed.rs
+++ crates/zed/src/zed.rs
@@ -4657,6 +4657,7 @@ mod tests {
                 "go_to_line",
                 "icon_theme_selector",
                 "journal",
+                "jump",
                 "keymap_editor",
                 "keystroke_input",
                 "language_selector",
