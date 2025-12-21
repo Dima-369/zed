13b2bf81d97255c41e136c2b01c4c0cbcb3beaca
Dima Butemann <dbutemann@gmail.com>
2025-12-21 13:16:14 +0100

    Improve recent files UI

--- crates/recent_files/src/lib.rs
+++ crates/recent_files/src/lib.rs
@@ -4,11 +4,16 @@ use gpui::{
     App, AsyncApp, Context, DismissEvent, Entity, EventEmitter, FocusHandle, Focusable,
     Subscription, Task, WeakEntity, Window,
 };
+use gpui::{px, Pixels};
 use ordered_float::OrderedFloat;
 use parking_lot::Mutex;
 use picker::{Picker, PickerDelegate};
-use std::path::{Path, PathBuf};
-use std::sync::Arc;
+use std::{
+    borrow::Cow,
+    ops::Range,
+    path::{Component, Path, PathBuf},
+    sync::Arc,
+};
 use ui::{HighlightedLabel, ListItem, ListItemSpacing, prelude::*};
 use util::paths::PathExt;
 use workspace::{
@@ -564,7 +569,7 @@ impl PickerDelegate for RecentFilesDelegate {
         &self,
         ix: usize,
         selected: bool,
-        _window: &mut Window,
+        window: &mut Window,
         cx: &mut Context<Picker<Self>>,
     ) -> Option<Self::ListItem> {
         let hit = self.matches.get(ix)?;
@@ -578,7 +583,7 @@ impl PickerDelegate for RecentFilesDelegate {
             .unwrap_or_default();
 
         let file_name_start = path_string.len().saturating_sub(file_name.len());
-        let dir_name = &path_string[0..file_name_start];
+        let mut dir_name = path_string[0..file_name_start].to_string();
 
         let file_name_highlights: Vec<usize> = hit
             .positions
@@ -587,12 +592,54 @@ impl PickerDelegate for RecentFilesDelegate {
             .map(|&i| i - file_name_start)
             .collect();
 
-        let dir_highlights: Vec<usize> = hit
+        let mut dir_highlights: Vec<usize> = hit
             .positions
             .iter()
             .filter(|&&i| i < file_name_start)
             .copied()
             .collect();
+
+        if dir_name.is_ascii() {
+            let max_width = rems(48.).to_pixels(window.rem_size());
+            let (normal_em, small_em) = {
+                let style = window.text_style();
+                let font_id = window.text_system().resolve_font(&style.font());
+                let font_size = ui::TextSize::Default
+                    .rems(cx)
+                    .to_pixels(window.rem_size());
+                let normal = cx
+                    .text_system()
+                    .em_width(font_id, font_size)
+                    .unwrap_or(px(16.));
+                let font_size = ui::TextSize::Small
+                    .rems(cx)
+                    .to_pixels(window.rem_size());
+                let small = cx
+                    .text_system()
+                    .em_width(font_id, font_size)
+                    .unwrap_or(px(10.));
+                (normal, small)
+            };
+            let budget = full_path_budget(&file_name, normal_em, small_em, max_width);
+
+            if budget > 0 && dir_name.len() > budget {
+                let components = PathComponentSlice::new(&dir_name);
+                if let Some(elided_range) = components.elision_range(budget - 1, &dir_highlights) {
+                    let elided_len = elided_range.end - elided_range.start;
+                    let placeholder = "…";
+                    dir_highlights.retain_mut(|mat| {
+                        if *mat >= elided_range.end {
+                            *mat -= elided_len;
+                            *mat += placeholder.len();
+                        } else if *mat >= elided_range.start {
+                            return false;
+                        }
+                        true
+                    });
+                    dir_name.replace_range(elided_range, placeholder);
+                }
+            }
+        }
 
         let file_icon = FileIcons::get_icon(&path, cx)
             .map(|icon| Icon::from_path(icon).color(Color::Muted));
@@ -612,12 +659,139 @@ impl PickerDelegate for RecentFilesDelegate {
                             file_name_highlights,
                         ))
                         .child(
-                            HighlightedLabel::new(dir_name.to_string(), dir_highlights)
+                            HighlightedLabel::new(dir_name, dir_highlights)
                                 .size(LabelSize::Small)
                                 .color(Color::Muted),
                         ),
                 ),
         )
+    }
+}
+
+fn full_path_budget(
+    file_name: &str,
+    normal_em: Pixels,
+    small_em: Pixels,
+    max_width: Pixels,
+) -> usize {
+    (((max_width / 0.8) - (file_name.len() as f32) * normal_em) / small_em) as usize
+}
+
+#[derive(Clone, Debug, PartialEq, Eq)]
+struct PathComponentSlice<'a> {
+    path: Cow<'a, Path>,
+    path_str: Cow<'a, str>,
+    component_ranges: Vec<(Component<'a>, Range<usize>)>,
+}
+
+impl<'a> PathComponentSlice<'a> {
+    fn new(path: &'a str) -> Self {
+        let trimmed_path = Path::new(path).components().as_path().as_os_str();
+        let mut component_ranges = Vec::new();
+        let mut components = Path::new(trimmed_path).components();
+        let len = trimmed_path.as_encoded_bytes().len();
+        let mut pos = 0;
+        while let Some(component) = components.next() {
+            component_ranges.push((component, pos..0));
+            pos = len - components.as_path().as_os_str().as_encoded_bytes().len();
+        }
+        for ((_, range), ancestor) in component_ranges
+            .iter_mut()
+            .rev()
+            .zip(Path::new(trimmed_path).ancestors())
+        {
+            range.end = ancestor.as_os_str().as_encoded_bytes().len();
+        }
+        Self {
+            path: Cow::Borrowed(Path::new(path)),
+            path_str: Cow::Borrowed(path),
+            component_ranges,
+        }
+    }
+
+    fn elision_range(&self, budget: usize, matches: &[usize]) -> Option<Range<usize>> {
+        let eligible_range = {
+            assert!(matches.windows(2).all(|w| w[0] <= w[1]));
+            let mut matches = matches.iter().copied().peekable();
+            let mut longest: Option<Range<usize>> = None;
+            let mut cur = 0..0;
+            let mut seen_normal = false;
+            for (i, (component, range)) in self.component_ranges.iter().enumerate() {
+                let is_normal = matches!(component, Component::Normal(_));
+                let is_first_normal = is_normal && !seen_normal;
+                seen_normal |= is_normal;
+                let is_last = i == self.component_ranges.len() - 1;
+                let contains_match = matches.peek().is_some_and(|mat| range.contains(mat));
+                if contains_match {
+                    matches.next();
+                }
+                if is_first_normal || is_last || !is_normal || contains_match {
+                    if longest
+                        .as_ref()
+                        .is_none_or(|old| old.end - old.start <= cur.end - cur.start)
+                    {
+                        longest = Some(cur);
+                    }
+                    cur = i + 1..i + 1;
+                } else {
+                    cur.end = i + 1;
+                }
+            }
+            if longest
+                .as_ref()
+                .is_none_or(|old| old.end - old.start <= cur.end - cur.start)
+            {
+                longest = Some(cur);
+            }
+            longest
+        };
+
+        let eligible_range = eligible_range?;
+        assert!(eligible_range.start <= eligible_range.end);
+        if eligible_range.is_empty() {
+            return None;
+        }
+
+        let elided_range: Range<usize> = {
+            let byte_range = self.component_ranges[eligible_range.start].1.start
+                ..self.component_ranges[eligible_range.end - 1].1.end;
+            let midpoint = self.path_str.len() / 2;
+            let distance_from_start = byte_range.start.abs_diff(midpoint);
+            let distance_from_end = byte_range.end.abs_diff(midpoint);
+            let pick_from_end = distance_from_start > distance_from_end;
+            let mut len_with_elision = self.path_str.len();
+            let mut i = eligible_range.start;
+            while i < eligible_range.end {
+                let x = if pick_from_end {
+                    eligible_range.end - i + eligible_range.start - 1
+                } else {
+                    i
+                };
+                len_with_elision -= self.component_ranges[x]
+                    .0
+                    .as_os_str()
+                    .as_encoded_bytes()
+                    .len()
+                    + 1;
+                if len_with_elision <= budget {
+                    break;
+                }
+                i += 1;
+            }
+            if len_with_elision > budget {
+                return None;
+            } else if pick_from_end {
+                let x = eligible_range.end - i + eligible_range.start - 1;
+                x..eligible_range.end
+            } else {
+                let x = i;
+                eligible_range.start..x + 1
+            }
+        };
+
+        let byte_range = self.component_ranges[elided_range.start].1.start
+            ..self.component_ranges[elided_range.end - 1].1.end;
+        Some(byte_range)
     }
 }
 
